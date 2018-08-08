64742bb7d0621f9693e2ae7d35e3ad43a69cf2e3
LeaEmma <35705043+LeaEmma@users.noreply.github.com>
2018-08-08 15:47:06 +0000

    Delete UpdateSolution.ps1

--- script SharePoint/UpdateSolution.ps1
+++
@@ -1,6 +0,0 @@
-﻿#Cargo los comandos de SharePoint para PowerShell
- if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) {
- Add-PSSnapin "Microsoft.SharePoint.PowerShell"
- } 
-
- Update-SPSolution -Identity MTEySS.GestionAdministrativa.wsp -LiteralPath C:\Users\LIRIARTE\Documents\MTEySS.GestionAdministrativa.wsp -GACDeployment
