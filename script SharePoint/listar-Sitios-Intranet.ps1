3c56fd43646fceaeeb7caedd2963729a388ea3e6
LeaEmma <35705043+LeaEmma@users.noreply.github.com>
2018-08-08 15:46:39 +0000

    Delete listar-Sitios-Intranet.ps1

--- script SharePoint/listar-Sitios-Intranet.ps1
+++
@@ -1,6 +0,0 @@
-﻿ #Add SharePoint PowerShell SnapIn if not already added
- if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) {
- Add-PSSnapin "Microsoft.SharePoint.PowerShell"
- } 
- 
- Get-SPWeb -site http://gesadmdesa.trabajo.gob.ar -Limit All | Select Title, @{Name="Url";Expression={ $_.RootFolder.ServerRelativeUrl }}
